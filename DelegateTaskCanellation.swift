aea7c2e2a01ba33a5567848eb4b286901f46af34
Bartlomiej Woronin <bartlomiej.woronin@roche.com>
2021-12-12 14:20:11 +0100

    Create DelegateTaskCanellation.swift

---
+++ DelegateTaskCanellation.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@MainActor
+class MyDelegate: UICollectionViewDelegate {
+    func collectionView(_ view: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt item: IndexPath) {
+        
+        var thumbnailsTasks: [IndexPath: Task<Void, Never>] = [:]
+        let ids = getThumbnailIDs(for: item)
+        
+        thumbnailsTasks[item] = Task {
+            defer { thumbnailsTasks[item] = nil  }
+            let thumbnails = await fetchThumbnails(for: ids)
+            display(thumbnails, in: cell)
+        }
+    }
+}
